51bab226b110d28b744e7fd476fd16beda55602f
Brandon Anzaldi <brandon@brandonanzaldi.com>
2015-03-28 21:22:47 -0700

    Fixing install and adding basic README

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-if [ $(id -u) == 0 ]; then
+if [ "$(id -u)" -eq 0 ]; then
   curl -Lo /usr/local/bin/getignore https://raw.githubusercontent.com/caffeinewriter/getignore/master/getignore.sh
   chmod +x /usr/local/bin/getignore
   echo "Installation has completed successfully."
 else
   echo "This script should be run as root, or with sudo permissions in order to write to /usr/bin/local."
 fi
-
