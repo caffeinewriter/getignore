e78d424895b21aa26b946abf032b0de0f86a7009
Brandon Anzaldi <brandon@brandonanzaldi.com>
2015-03-28 21:01:04 -0700

    Initial commit of getignore

---
+++ getignore.sh
@@ -0,0 +1,13 @@
+PWD_GITIGNORE="$(pwd)/.gitignore"
+if [ ! -f $PWD_GITIGNORE ]; then 
+  echo "Getting $1.gitignore"
+  curl -s -f -o $PWD_GITIGNORE "https://raw.githubusercontent.com/github/gitignore/master/$1.gitignore"
+  if [ -f $PWD_GITIGNORE ]; then
+    echo "Gitignore $1.gitignore fetched, and written to .gitignore successfully."
+  else
+    echo "Unable to fetch and write $1.gitignore. It may not exist, or this script may be unable to write to the current directory. Check http://github.com/github/gitignore to ensure that the specified .gitignore file exists, and that your capitalization is correct."
+  fi
+else
+  echo "A .gitignore file already exists in this directory. Please remove or rename it, and then run this again."
+fi
+
